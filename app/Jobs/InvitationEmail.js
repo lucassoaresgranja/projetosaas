803dbfef7a9f9e16a05589a860a65b3c8459b52a
Lucas <lucassoaresgranja>
2020-08-24 12:36:14 -0300

    teste de aplicação

---
+++ app/Jobs/InvitationEmail.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const Mail = use('Mail')
+
+class InvitationEmail {
+  // If this getter isn't provided, it will default to 1.
+  // Increase this number to increase processing concurrency.
+  static get concurrency () {
+    return 1
+  }
+
+  // This is required. This is a unique key used to identify this job.
+  static get key () {
+    return 'InvitationEmail-job'
+  }
+
+  // This is where the work is done.
+  async handle ({user, team, email}) {
+      await Mail.send(
+        ['emails.invitation'],
+        { team: team.name, user: user.name },
+        message =>{
+          message
+              .to(email)
+              .from('lucassoaresgranja@yahoo.com.br', 'Lucas | Infoluc')
+              .subject(`Convite para o Time ${team.name}`)
+        }
+      )
+  }
+}
+
+module.exports = InvitationEmail
+
---
+++ app/Validators/Invite.js
@@ -0,0 +1,17 @@
+'use strict'
+
+class Invite {
+
+  get validateAll(){
+    return true
+  }
+
+  get rules () {
+    return {
+      invites: 'required|array',
+      'invites.*': 'required|email'
+    }
+  }
+}
+
+module.exports = Invite
